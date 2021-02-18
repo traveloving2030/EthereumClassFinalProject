12aa68d519e684abeab8592080f0dcee6b470c81
jiwon <traveloving2030@gmail.com>
2021-02-18 12:58:47 +0900

    123

--- router/FindTutor.js
+++ router/FindTutor.js
@@ -13,3 +13,22 @@ var contract = new web3.eth.Contract(ABI, CA);
 router.get('/', function (req, res, next) {
     res.render('FindTutor', { title: "FindTutor" });
 });
+
+router.post('/', function (req, res) {
+    const accounts = await web3.eth.getAccounts();
+    var total = new Array(); //전체 선생님들 정보를 저장하기 위한 임시 배열
+    var array2 = new Array(); //전체 선생님들 중 특정 과목과 성별을 만족하는 선생님들을 필터링하여 저장하기 위한 임시 배열
+
+    const totalTutors = await storehash.methods.getNumOfTutors().call();
+    
+    for (var i = 0; i < totalTutors; i++) {
+        const tutorInfo = await storehash.methods.getTutorInfo(i).call();
+        total.push(tutorInfo);
+    }
+    total.map(tutors, function(){
+        if(tutors[1]==req.body.gender && )
+    })
+
+});
+
+module.exports = router;
