fcf9601fb891ee9901409c78769df209b6268c21
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2021-02-26 17:44:35 +0900

    Update WhisperAPI.js

--- router/API/WhisperAPI.js
+++ router/API/WhisperAPI.js
@@ -1,37 +1,77 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
+const defaultRecipientPubKey = "0x04ffb2647c10767095de83d45c7c0f780e483fb2221a1431cb97a5c61becd3c22938abfe83dd6706fc1154485b80bc8fcd94aea61bf19dd3206f37d55191b9a9c4";
+const defaultTopic = "0x5a4ea131";
+
+
+// 1. key 생성
+// 2. Filter 생성
+// 3. 메세지 post 요청
 
 const WhisperAPI = {
-
-
-    WhisperChatSender: async (request) => {
-        try{
-            //실습시 바꿔줘야함
-            // const keypair="227b814af4b372faeaf01b65d36217ec8c20d0463a55c4ee677555cc222df599";
-            // const Receiversymkey = "94b298316f8f0773a7f4d16ad26599ea7cd7a06bf544ea931599c725c3a5b192";
-            const symkey = await  web3.shh.newSymKey();
-            const keypair= await web3.shh.newKeyPair();
-            const subscription = await web3.shh.subscribe("messages",{
-                symKeyID: symkey,
-                topics: ['0xffaadd11']
-            })
-            
-             let postData = {
-                symKeyID: symkey,
-                sig: keypair,
-                ttl: 20,
-                topic: '0xffaadd11',
-                powTarget: 2.01,
-                powTime: 100,
-                payload: web3.utils.fromAscii(request.sendermsg)
-            };
     
-            const result=await web3.shh.post(postData); 
-            // const result=await web3.utils.toUtf8(hexresult)
+    //request : name, text
+    configWithKey: async () => {
+       
+        let msgs=new Array();
+
+        const asymKeyId = await web3.shh.newKeyPair();
+        const asymPubKey = await web3.shh.getPublicKey(asymKeyId);
+        
+        let filter = {
+            topics: ['0x07678231'],
+            privateKeyID=asymKeyId
+        };
+       
+        const msgFilter=await web3.shh.newMessageFilter(filter);
+
+        
+        
+        //이건.. 계속 메세지 받아서 뿌려주는거니까 router단에서 해줘야할거 같은데
+        setInterval(() => {
+            web3.shh.getFilterMessages(msgFilter).then(messages => {
+                for (let element of messages) {
+                    let Hexmessage = await web3.utils.fromAscii(element.payload);
+                    let message=await web3.utils.toUtf8(Hexmessage)
+                    msgs.push(message);
+                }
+            });
+        }, 1000);
+
+
+
+
+    },
+
+    //request : asym(checkbox t/f유무), asymKeyId, sendermsg, name , recipientPubKey(복붙하는 상대방 공개키)
+    SendMessage: async (request) => {
+        try {
+            let msgs=new Array();
+
+            let msg = request.sendermsg;
+            
+            msgs.push(msg);
+
+ 
+
+            let postData = {
+                pubkey: request.recipientPubKey,
+                sig: request.asymKeyId,
+				ttl: 7,
+				topic: '0x07678231',
+				powTarget: 2.01,
+				powTime: 100,
+                payload: await web3.utils.fromAscii(request.sendermsg)
+            };
+
+            
+            //결과 확인해보기
+            const result = await web3.shh.post(postData);
+            
 
             return result
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
 
@@ -39,7 +79,7 @@ const WhisperAPI = {
     },
 
     WhisperChatReceiver: async (request) => {
-        try{
+        try {
             const SenderkeyPair = await web3.shh.newKeyPair();
             const SenderpubKey = await web3.shh.getPublicKey(SenderkeyPair);
             await web3.shh.newMessageFilter()
@@ -51,10 +91,10 @@ const WhisperAPI = {
                 payload: web3.utils.fromAscii(request.recivermsg),
             };
 
-            const hexresult=await web3.shh.post(postData);
-            const result=await web3.utils.hexToUtf8(hexresult)
+            const hexresult = await web3.shh.post(postData);
+            const result = await web3.utils.hexToUtf8(hexresult)
             return result
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
 
