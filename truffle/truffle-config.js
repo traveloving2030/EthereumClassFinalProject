8e731dfa3ab3d1d37cfb9c1b7aeaabe8b632f2bd
jiwon <traveloving2030@gmail.com>
2021-07-15 15:14:24 +0900

    123

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -5,7 +5,8 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "123" //Geth 네트워크 아이디
+      network_id: "123",
+      gas: 4000000
     }
   }
 };
