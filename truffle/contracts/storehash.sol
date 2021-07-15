24da8df0c891ac9b9685e6e46e7d8942262aca6f
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2021-07-15 12:51:38 +0900

    1

--- truffle/contracts/storehash.sol
+++ truffle/contracts/storehash.sol
@@ -2,7 +2,6 @@
 //0xb84b12e953f5bcf01b05f926728e855f2d4a67a9  contract address on rinkeby
 //deployed using remix 
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
 
 contract Contract {
 
