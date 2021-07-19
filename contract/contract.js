017995b263da1213e174b33c25806d25fc9c6e97
jiwon <traveloving2030@gmail.com>
2021-07-19 13:35:31 +0900

    1

--- contract/contract.js
+++
@@ -1,8 +0,0 @@
-
-
-//access our local copy to contract deployed on rinkeby testnet
-//use your own contract address
-const address = '0x47cb4093B34fC92983E520E2D1b54BF407dDDe99';
-//use the ABI from your contract
-const ABI = require('../truffle/build/contracts/Contract').abi
-module.exports={address, ABI};
